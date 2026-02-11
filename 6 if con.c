99b9f95fb285ad59f78ced1569de30c12cf8efda
rohithkumarr930-code <rohithkumarr930@gmail.com>
2026-02-11 15:14:33 +0530

    Add files via upload

---
+++ 6 if con.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='a')
+    {
+        printf("vowel");
+    }
+    else if(ch=='e')
+    {
+        printf("vowel");
+    }
+    else if(ch=='i')
+    {
+        printf("vowel");
+    }
+    else if(ch=='0')
+    {
+        printf("vowel");
+    }
+    else if(ch=='u')
+    {
+        printf("vowel");
+    }
+    else
+    {
+        printf("consonent");
+    }
+    return 0;
+}
