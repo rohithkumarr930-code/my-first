5ed303c8cf3d2b56259c3c46d2272cfde6fea761
rohithkumarr930-code <rohithkumarr930@gmail.com>
2026-02-11 13:55:27 +0530

    Add files via upload

---
+++ 4 if con.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(b>a)
+    {
+        printf("20 is greater");
+    }
+    else
+    {
+        printf("lesser");
+    }
+    return 0;
+}
+    s
