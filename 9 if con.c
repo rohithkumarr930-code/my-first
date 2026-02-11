251a83abe2a7fafe9cb72b28b2f6327ae1c1a68a
rohithkumarr930-code <rohithkumarr930@gmail.com>
2026-02-11 15:29:00 +0530

    Add files via upload

---
+++ 9 if con.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=18)
+    {
+        printf("Eligible to vote");
+    }
+    else
+    {
+        printf("not eligible to vote");
+    }
+    return 0;
+    
+}
