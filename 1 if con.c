de24651094b43cfedf131285190af982706ec449
rohithkumarr930-code <rohithkumarr930@gmail.com>
2026-02-11 13:36:08 +0530

    Add files via upload

---
+++ 1 if con.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    scanf("%d",&number);
+    if(number>0)
+    {
+        printf("positive");
+    }
+    else if(number<0)
+    {
+        printf("negative");
+    }
+    else
+    {
+        printf("zero");
+    }
+
+    return 0;
+}
