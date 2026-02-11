0fbe11fb9e4aebe8f3c74d99d58bea7af91fa406
rohithkumarr930-code <rohithkumarr930@gmail.com>
2026-02-11 16:09:54 +0530

    Add files via upload

---
+++ 14 if con.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num%11==0)
+    {
+        printf("Divisible by 11");
+    }
+    else
+    {
+        printf("not divisible by 11");
+    }
+    return 0;
+        
+}
