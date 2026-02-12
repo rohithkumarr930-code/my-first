28326482f60b7c647ff8e7a22034f5fc5a1cbfc0
rohithkumarr930-code <rohithkumarr930@gmail.com>
2026-02-12 10:01:40 +0530

    Add files via upload

---
+++ 16 if con.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a==b&&b==c&&c==a)
+    {
+        printf("Equilateral triangle");
+    }
+    else
+    {
+        printf("not equilateral triangle");
+    }
+    return 0;
+}
