f7ba22c00298dd6a51af6cbdda41435488e5f693
rohithkumarr930-code <rohithkumarr930@gmail.com>
2026-02-12 14:48:29 +0530

    Add files via upload

---
+++ 21 if con.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char operation;
+    scanf("%c",&operation);
+    int a,b;
+    scanf("%d%d",&a,&b);
+    switch(operation)
+    {
+        case'+':
+        printf("result=%d",a+b);
+        break;
+        case'-':
+        printf("result=%d",a-b);
+        break;
+        case'*':
+        printf("result=%d",a*b);
+        break;
+        case'/':
+        printf("result=%d",a/b);
+        break;
+        default:
+        printf("enter valid opeations");
+        break;
+    }
+
+    return 0;
+}
