eac6cb1188948335f74a2357989d433d943a8d89
rohithkumarr930-code <rohithkumarr930@gmail.com>
2026-02-12 13:35:05 +0530

    Add files via upload

---
+++ 23 if con.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+    scanf("%d",&month);
+    if(month==1||month==3||month==5||month==7||month==9||month==11)
+    {
+        printf("31 days");
+    }
+    else if(month==4||month==6||month==8||month==10||month==12)
+    {
+        printf("30 days");
+    }
+    else if(month==2)
+    {
+        printf("28 or 29 days");
+    }
+
+    return 0;
+}
