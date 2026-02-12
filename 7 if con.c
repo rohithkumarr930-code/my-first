dce61a1ef5c903f934c07be047f1c1e36c0fefa2
rohithkumarr930-code <rohithkumarr930@gmail.com>
2026-02-12 09:55:17 +0530

    Add files via upload

---
+++ 7 if con.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf("%c",&ch);
+    if(ch>=65&&ch<=90)
+    {
+        printf("Alphabet");
+    }
+    else if(ch>=97&&ch<=122)
+    {
+        printf("Alphabet");
+    }
+    else if(ch>=48&&ch<=57)
+    {
+        printf("Digit");
+    }
+    else
+    {
+        printf("Special character");
+    }
+    return 0;
+}
