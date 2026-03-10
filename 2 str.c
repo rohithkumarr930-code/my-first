ab1e20cdc5c4f0d692508bebcb13403a4b1c70dc
rohithkumarr930-code <rohithkumarr930@gmail.com>
2026-03-10 15:53:54 +0530

    Add files via upload

---
+++ 2 str.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    char password[100];
+    int i=0;
+    int hasUpper=0;
+    scanf("%s",password);
+    while(password[i]!='\0')
+    {
+        if(password[i]>='A'&&password[i]<='Z')
+        {
+            hasUpper=1;
+            break;
+        }
+        i++;
+    }
+    if(hasUpper==1)
+    {
+        printf("Valid");
+    }
+    else
+    {
+        printf("Invalid");
+    }
+    return 0;
+}
